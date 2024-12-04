3a7b5920d7131871f681d5f93c9e0aecdaeb4c65
Nephew20 <cofieldk20@gmail.com>
2024-12-03 20:09:53 -0500

    updated draft schema and developed seed data

--- db/schema.sql
+++ db/schema.sql
@@ -4,26 +4,29 @@ CREATE DATABASE ge_db;
 USE ge_db;
 
 CREATE TABLE departments (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(30) NOT NULL,
-)
+    PRIMARY KEY(id)
+);
 
 CREATE TABLE roles (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL,
     job_title VARCHAR(30) NOT NULL,
     departments_id INT NOT NULL,
     salary INT NOT NULL,
+    PRIMARY KEY(id),
     FOREIGN KEY (departments_id)
     REFERENCES departments(id)
-)
+);
 
 CREATE TABLE employees (
     id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    managers VARCHAR(30) NOT NULL,
+    manager VARCHAR(30) NOT NULL,
     roles_id INT NOT NULL,
+    PRIMARY KEY(id),
     FOREIGN KEY (roles_id)
     REFERENCES roles(id)
-)
+);
 
---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO departments(name) 
+VALUES  ("Emergency Department"),
+        ("Operating Room"),
+        ("Radiology"),
+        ("Cardiology");
+
+INSERT INTO roles(job_title, salary)
+VALUES  ("Apprentice", 45000),
+        ("BMET Technician 1", 55000),
+        ("BMET Technician 2", 70000);
+
+INSERT INTO employees(first_name, last_name, manager)
+VALUES  ("Rodger", "Rabbit", "Jessica Rabbit"),
+        ("Daffy", "Duck", "Bugs Bunny"),
+        ("Wile E.", "Coyote", "Road Runner"),
+        ("Tweety", "Bird", "Sylvestor")
+         
