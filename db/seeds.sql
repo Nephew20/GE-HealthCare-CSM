233222c35a76653b1315fc1a4e159b06253fe78a
Nephew20 <cofieldk20@gmail.com>
2024-12-12 14:20:02 -0500

    completed seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,16 +2,20 @@ INSERT INTO departments(name)
 VALUES  ("Emergency Department"),
         ("Operating Room"),
         ("Radiology"),
-        ("Cardiology");
+        ("Cardiology"),
+        ("General Patient Care");
 
-INSERT INTO roles(job_title, salary)
-VALUES  ("Apprentice", 45000),
-        ("BMET Technician 1", 55000),
-        ("BMET Technician 2", 70000);
+INSERT INTO roles(job_title, departments_id, salary)
+VALUES  ("Apprentice", 5, 45000),
+        ("BMET Technician 1", 2, 55000),
+        ("BMET Technician 2", 3, 70000),
+        ("Field Service Engineer 1", 4, 65000),
+        ("Field Service Engineer 2", 2, 80000);
 
-INSERT INTO employees(first_name, last_name, manager)
-VALUES  ("Rodger", "Rabbit", "Jessica Rabbit"),
-        ("Daffy", "Duck", "Bugs Bunny"),
-        ("Wile E.", "Coyote", "Road Runner"),
-        ("Tweety", "Bird", "Sylvestor")
+INSERT INTO employees(first_name, last_name, roles_id, manager)
+VALUES  ("Rodger", "Rabbit", 1, "Jessica Rabbit"),
+        ("Daffy", "Duck", 2, "Bugs Bunny"),
+        ("Wile E.", "Coyote", 3, "Road Runner"),
+        ("Tweety", "Bird", 4, "Sylvestor"),
+        ("Slowpoke", "Martinez", 5, "Speedy Gonzales");
          
