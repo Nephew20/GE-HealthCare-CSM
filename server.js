4af3b78b1ca0d33f7002caca896309129352672d
Nephew20 <cofieldk20@gmail.com>
2024-12-12 14:56:56 -0500

    imported and established connection to the mysql database

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
+// Imports
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
 
 inquirer
   .prompt([
     {
         type: 'list',
-        name: "options",
+        name: "option",
         message: "Hello, What would you like to do?",
         choices: ["View all departments", "View all employees", "Add a department", 
             "Add a role", "Add an employee", "Update an employee role"],    
     }
   ])
   .then((answers) => {
-    console.log(answers)
+    const db = mysql.createConnection(
+      {
+        host: "localhost",
+        user: "root",
+        password: "12345",
+        database: "ge_db"
+      }
+    )
+
+    if (answers.option == "View all departments") {
+      db.query(`SELECT * FROM departments;`)
+    }
+    
+    console.log(answers.option)
   })
   .catch((error) => {
     if (error.isTtyError) {
