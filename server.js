f0f50a43e3e860709b30186bc9dcfc0fc68a0e87
Nephew20 <cofieldk20@gmail.com>
2024-12-17 18:55:23 -0500

    developed query for viewing all employees

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use(express.json());
 inquirer
   .prompt([
     {
-        type: 'list',
-        name: "option",
-        message: "Hello, What would you like to do?",
-        choices: ["View all departments", "View all roles", "View all employees", "Add a department", 
-            "Add a role", "Add an employee", "Update an employee role"],    
+      type: 'list',
+      name: "option",
+      message: "Hello, What would you like to do?",
+      choices: ["View all departments", "View all roles", "View all employees", "Add a department",
+        "Add a role", "Add an employee", "Update an employee role"],
     }
   ])
   .then((answers) => {
@@ -27,7 +27,7 @@ inquirer
         // Your Mysql User
         user: "root",
         // Your Mysql Password
-        password: "",
+        password: "12345",
         database: "ge_db"
       }
     )
@@ -49,18 +49,18 @@ inquirer
       })
       return
     } else if (answers.option == "View all employees") {
-      db.query(``, (err, result) => {
+      db.query(`SELECT employees.id, employees.first_name, employees.last_name, roles.job_title, roles.salary, departments.name AS department_name, employees.manager AS employee_manager FROM employees JOIN roles ON roles.id = employees.roles_id JOIN departments ON departments.id = roles.departments_id`, (err, result) => {
         if (err) {
           console.log(err)
         }
-        console.log(result)
+        console.table(result)
       })
       return
     }
-    
+
   })
   .catch((error) => {
     if (error.isTtyError) {
-        console.log(error)
+      console.log(error)
     }
   });
